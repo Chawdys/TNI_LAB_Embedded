06ffefb95dda35cb08e7d42a9871f1ece7fb0793
Hewkaw02 <53098752+Hewkaw02@users.noreply.github.com>
2022-02-11 14:18:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -392,6 +392,10 @@ void play_music(void){
 	}
 }
 
-
+void LED_TOGGLE(void){
+	LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_7);
+	LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_5);
+	LL_GPIO_TogglePin(GPIOB, LL_GPIO_PIN_4);
+}
 
 
